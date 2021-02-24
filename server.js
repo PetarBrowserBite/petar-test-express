8169d4b3b49b59f7043471b7ace5b9dc38b85b1e
Petar <petar@browserbite.io>
2021-02-24 11:32:51 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/show", function (req, res) {
+  res.send("Hello World!");
+});
+
+app.get("/", function (req, res) {
+    res.send("Works ! ");
+  });
+
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`);
+});
